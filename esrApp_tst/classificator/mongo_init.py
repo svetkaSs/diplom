f6316d781b5455a00bf59a2246d2d31c8c3d7de7
orisborlov <mrwiggle40000@gmail.com>
2023-11-19 18:49:10 +0700

    feat(): chamgedmongo to cloud claster

--- esrApp_tst/classificator/mongo_init.py
+++ esrApp_tst/classificator/mongo_init.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 def mongoClient():
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING = "mongodb://localhost:27017"
+    CONNECTION_STRING = 'mongodb+srv://mrwiggle40000:OErZka7OiZTiToGx@cluster0.dt0bgxc.mongodb.net/incubator'
 
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     client = MongoClient(CONNECTION_STRING)
--- esrApp_tst/mainEsr.py
+++ esrApp_tst/mainEsr.py
@@ -24,7 +24,7 @@ class AudioRecognition:
       def get_database():
       
          # Provide the mongodb atlas url to connect python to mongodb using pymongo
-         CONNECTION_STRING = "mongodb://localhost:27017"
+         CONNECTION_STRING ='mongodb+srv://mrwiggle40000:OErZka7OiZTiToGx@cluster0.dt0bgxc.mongodb.net/incubator'
       
          # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
          client = MongoClient(CONNECTION_STRING)
